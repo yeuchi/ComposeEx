d1510d5173801c3b3d9813853b924ed0783d77d3
Chi Yeung <yeun0007@umn.edu>
2021-03-07 15:11:35 -0600

    functional hello world
    Whoa ... Google tutorial does not work on beta.  Text() is not supported in the foundation library.  Revert back to alpha for functionality.

--- app/src/main/java/com/ctyeung/composeex/MainActivity.kt
+++ app/src/main/java/com/ctyeung/composeex/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.ctyeung.composeex
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.w3c.dom.Text as Text
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setContent {
+            MaterialTheme {
+                    TopAppBar(title = {
+                        Text("Hello World")
+                    })
+
+                Column(modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text("Hello World!", style = TextStyle(fontStyle = FontStyle.Italic, fontSize = 30.sp, color = Color.Green))
+                }
+            }
+        }
+    }
+
+    @Composable
+    fun Greeting(name: String) {
+        Text (text = "Hello $name!")
     }
 }
