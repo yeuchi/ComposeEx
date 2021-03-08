80a527141e4ae25a17c7ca67ba87d661cb5db4c8
Chi Yeung <yeun0007@umn.edu>
2021-03-07 20:18:40 -0600

    clickableText

--- app/src/main/java/com/ctyeung/composeex/MainActivity.kt
+++ app/src/main/java/com/ctyeung/composeex/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.ctyeung.composeex
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.ClickableText
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
@@ -12,6 +14,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.unit.sp
@@ -26,10 +29,13 @@ class MainActivity : AppCompatActivity() {
                         Text("Hello World")
                     })
 
-                Column(modifier = Modifier.fillMaxWidth().fillMaxHeight(),
+                Column(modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(),
                     verticalArrangement = Arrangement.Center,
                     horizontalAlignment = Alignment.CenterHorizontally) {
-                    Text("Hello World!", style = TextStyle(fontStyle = FontStyle.Italic, fontSize = 30.sp, color = Color.Green))
+                    ClickableText(text = AnnotatedString.Builder("Hello").toAnnotatedString(), onClick = { Toast.makeText(this@MainActivity.applicationContext, "You clicked me", Toast.LENGTH_LONG).show() })
+                    val txt = Text("Hello World!", style = TextStyle(fontStyle = FontStyle.Italic, fontSize = 30.sp, color = Color.Green))
                 }
             }
         }
